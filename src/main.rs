5959c7308d58698e6db84d932c18166eae4d0972
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-11-01 20:44:31 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use mail_parser::Message;
+use std::{fs, path::PathBuf};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(short, long)]
+    input: PathBuf,
+
+    #[structopt(short, long)]
+    output: Option<PathBuf>,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    let mut input = fs::read(&opt.input).expect(&format!("Unable to read file {:?}", opt.input));
+
+    let message = Message::parse(&mut input[..]);
+
+    let json_string = serde_json::to_string_pretty(&message)
+        .map_err(|e| panic!("Unable to generate JSON from email with error: {}", e))
+        .unwrap();
+
+    if let Some(output) = opt.output {
+        fs::write(output, json_string)
+            .map_err(|e| panic!("Unable to write to file with error: {}", e))
+            .unwrap();
+    } else {
+        println!("{}", json_string);
+    }
+}
