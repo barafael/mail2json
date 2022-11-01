4d45eb782b5cfe9a652d24de0c6c71ceeacfa107
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-11-01 15:40:52 +0100

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use clap::StructOpt;
+use clap::Parser;
 use clap_derive::Parser;
 use mail_parser::Message;
 use std::{fs, path::PathBuf};
 
 #[derive(Debug, Parser)]
+#[command(author, version)]
 struct Args {
-    #[clap(short, long)]
+    /// Input file.
+    #[arg(short, long)]
     input: PathBuf,
 
-    #[clap(short, long)]
+    /// Output file, if not given, `stdout` is used.
+    #[arg(short, long)]
     output: Option<PathBuf>,
 }
 
@@ -17,7 +20,7 @@ fn main() {
     let input =
         fs::read(&opt.input).unwrap_or_else(|_| panic!("Unable to read file {:?}", opt.input));
 
-    let message = Message::parse(&input[..]);
+    let message = Message::parse(&input[..]).expect("Failed to parse message");
 
     let json_string = serde_json::to_string_pretty(&message)
         .map_err(|e| panic!("Unable to generate JSON from email with error: {}", e))
